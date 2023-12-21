8c0424db8b7677b7a75ade1926731e03b1a52319
Data4i <paulobiahu@gmail.com>
2023-12-21 11:30:32 +0100

    implemented the algorithm to count the number of elements in an array using recursion

--- Chapter4 -> QuickSort/recursion_excercises.py
+++ Chapter4 -> QuickSort/recursion_excercises.py
@@ -1,3 +1,4 @@
+# Write out a recursive code to sum numbers in an array
 def sum_arr(arr):
     if not arr:
         return 0
@@ -8,3 +9,12 @@ print(sum_arr([2, 3, 4]))
 print(sum_arr([3]))
 print(sum_arr([]))
 
+
+# Write out a recursive function to count the number of items in a list
+def count_arr(arr):
+    if not arr:
+        return 0
+    return 1 + count_arr(arr[1:])
+
+
+print(count_arr([1, 2, 3, 4, 5, 3, 3, 3]))
