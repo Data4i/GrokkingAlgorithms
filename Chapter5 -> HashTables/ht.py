cc4e0d340ec2c6c09a29e5168996d6d760bab16e
Data4i <paulobiahu@gmail.com>
2023-12-25 16:43:46 +0100

    Created a simple voting checker using the .get() function of dictionaries

--- Chapter5 -> HashTables/ht.py
+++ Chapter5 -> HashTables/ht.py
@@ -5,4 +5,18 @@ product_price = {'Apple': 0.67, 'milk': 1.49, 'Avocado': 1.49}
 print(product_price)
 print(product_price['Avocado'])
 
+# Let's implement a simple voting system
+voted = {}
 
+
+def check_voted(name):
+    if voted.get(name):
+        print(f'Kick {name} Out!!!')
+    else:
+        voted[name] = True
+        print(f'Let {name} Vote')
+
+
+check_voted('Sam')
+check_voted('Paul')
+check_voted('Sam')
